f66e72a45fadd731d326ddc7341d94e76f029204
mehar sulaiman <dr.love9026@gmail.com>
2023-08-04 10:38:54 +0500

    styled components learning point

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
+import GlobalStyles from './styles/GlobalStyles';
+
+import Input from './ui/Input';
+import Button from './ui/Button';
+
+const H1 = styled.h1`
+  font-size: 90px;
+  background-color: olive;
+  font-weight: 700;
+`;
 
 export default function App() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <div>
+        <H1>Hello World</H1>
+        <Button>Click me</Button>
+        <Input placeholder='Enter value' />
+      </div>
+    </>
+  );
 }

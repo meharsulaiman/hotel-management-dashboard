5e6485c54f4b83d5f734f2e7f272f4cf727c50fa
mehar sulaiman <dr.love9026@gmail.com>
2023-08-05 10:34:37 +0500

    layout and some styling of sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,38 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
 import GlobalStyles from './styles/GlobalStyles';
 
-import Input from './ui/Input';
-import Button from './ui/Button';
-import Heading from './ui/Heading';
-import styled from 'styled-components';
-import Row from './ui/Row';
-
-const StyledApp = styled.div`
-  padding: 20px;
-`;
+import Account from './pages/Account';
+import Bookings from './pages/Bookings';
+import Cabins from './pages/Cabins';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Settings from './pages/Settings';
+import Users from './pages/Users';
+import PageNotFound from './pages/PageNotFound';
+import AppLayout from './ui/AppLayout';
 
 export default function App() {
   return (
     <>
       <GlobalStyles />
-      <StyledApp>
-        <Row type='horizontal'>
-          <Heading as='h1'>The Wild Oasis</Heading>
-          <div>
-            <Heading as='h2'>Check in and out</Heading>
-            <Button>Check in</Button>
-            <Button variation='danger' size='small'>
-              Check out
-            </Button>
-          </div>
-        </Row>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to='dashboard' />} />
+            <Route path='dashboard' element={<Dashboard />} />
+            <Route path='account' element={<Account />} />
+            <Route path='bookings' element={<Bookings />} />
+            <Route path='cabins' element={<Cabins />} />
+            <Route path='settings' element={<Settings />} />
+            <Route path='users' element={<Users />} />
+          </Route>
 
-        <Row>
-          <Heading as='h3'>Form</Heading>
-          <form action=''>
-            <Input placeholder='Enter value' />
-            <Input placeholder='Enter value' />
-          </form>
-        </Row>
-      </StyledApp>
+          <Route path='login' element={<Login />} />
+          <Route path='*' element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }

0ba585d87f39d4de9962d02b9c305bcde9752d5d
mehar sulaiman <sulaimanshabbir31@gmail.com>
2023-09-23 16:26:22 +0500

    pagination

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -4,9 +4,10 @@ import Menus from '../../ui/Menus';
 import Empty from '../../ui/Empty';
 import Spinner from '../../ui/Spinner';
 import { useBookings } from './useBooking';
+import Pagination from '../../ui/Pagination';
 
 function BookingTable() {
-  const { bookings, isLoading } = useBookings();
+  const { bookings, isLoading, count } = useBookings();
 
   if (isLoading) {
     return <Spinner />;
@@ -34,6 +35,9 @@ function BookingTable() {
             <BookingRow key={booking.id} booking={booking} />
           )}
         />
+        <Table.Footer>
+          <Pagination count={count} />
+        </Table.Footer>
       </Table>
     </Menus>
   );
--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,11 +1,13 @@
 import { getToday } from '../utils/helpers';
+import { PAGE_SIZE } from '../utils/constants';
 import supabase from './supabase';
 
-export async function getBookings({ filter, sortBy }) {
+export async function getBookings({ filter, sortBy, page }) {
   let query = supabase
     .from('bookings')
     .select(
-      'id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)'
+      'id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)',
+      { count: 'exact' }
     );
 
   // FILTER
@@ -20,13 +22,21 @@ export async function getBookings({ filter, sortBy }) {
     });
   }
 
-  let { data, error } = await query;
+  // PAGE
+  if (page) {
+    const from = (page - 1) * PAGE_SIZE;
+    const to = from + PAGE_SIZE - 1;
+
+    query = query.range(from, to);
+  }
+
+  let { data, error, count } = await query;
   if (error) {
     console.error(error);
     throw new Error('Bookings could not be found');
   }
 
-  return data;
+  return { data, count };
 }
 
 export async function getBooking(id) {
